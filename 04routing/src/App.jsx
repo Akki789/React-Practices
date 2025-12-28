5a2e747563d7f5f32cec43eef76fd630d3a95bbc
Akash <aakashkumarag96@gmail.com>
2025-12-28 12:54:43 +0530

    Project setup for routing practice

---
+++ 04routing/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="app-shell">
+      <div className="assignment-card">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App;
