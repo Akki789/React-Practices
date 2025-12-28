a3c8f902d4db8a289dab4deb66f84e5c56db39a7
Akash <aakashkumarag96@gmail.com>
2025-12-28 10:54:30 +0530

    Practiced question on use Effect with fetching API

--- 02use_effect/src/App.jsx
+++ 02use_effect/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import UsersInfo from "./components/UsersInfo";
 
 function App() {
 
   return (
     <div className="app-shell">
       <div className="assignment-card">
-        
+        <h2>Users info print using API</h2>
+        <UsersInfo />
       </div>
     </div>
   )
---
+++ 02use_effect/src/components/UsersInfo.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+export default function UsersInfo() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() =>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setUsers(data));
+    })
+  return (
+    <div>
+        <ul>
+            {users.map((u) => (
+                <li key={u.id}>{u.name}</li>
+            ))}
+        </ul>
+    </div>
+  )
+}
