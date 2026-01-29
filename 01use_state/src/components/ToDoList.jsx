6b665ae78792002f97760ca95ee9ffa01a07bb48
Akash <aakashkumarag96@gmail.com>
2026-01-29 19:53:54 +0530

    Added a delete button on each ToDo

--- 01use_state/src/components/ToDoList.jsx
+++ 01use_state/src/components/ToDoList.jsx
@@ -1,26 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function ToDoList() {
-    const[todo, setTodo] = useState([]);
-    const[task, setTask] = useState("");
+  const [todo, setTodo] = useState([]);
+  const [task, setTask] = useState("");
 
+  function addTodo() {
+    if (!task.trim()) return;
+    
+    setTodo([...todo, task]);
+    setTask("");
+  }
 
-    function addTodo(){
-        setTodo([...todo, task])
-        setTask("")
-    }
+  function handleDelete(index){
+    const updatedTodo =
+    todo.filter((_, i) => (
+        i != index
+    ));
+
+    setTodo(updatedTodo);
+  }
   return (
     <div>
-        <input type="text" placeholder='Add todo...' onChange={e => setTask(e.target.value)} />
+      <div>
+        <input
+          type="text"
+          placeholder="Add todo..."
+          onChange={(e) => setTask(e.target.value)}
+        />
         <button onClick={addTodo}>Add ToDo</button>
+      </div>
 
-       {
-        todo.map((l,i) => (
-            <ul>
-                <li key={i}>{l}</li>
-            </ul>
-        ))
-       }
+      <ul>
+        {todo.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => handleDelete(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
